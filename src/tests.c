fa901ae838de5acd1ac3e64a4e7f3a5a140f0fc1
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2024-01-02 21:26:45 +0300

    test 3

--- src/tests.c
+++ src/tests.c
@@ -26,16 +26,20 @@ bool test_2(void * start_heap) {
 
     if (malloc_1 == NULL) {
         printf("Test 2 failed: first of two blocks didn't allocate. \n");
+        _free(malloc_2);
         return false;
     }
 
     if (malloc_2 == NULL) {
         printf("Test 2 failed: last of two blocks didn't allocate. \n");
+        _free(malloc_1);
         return false;
     }
 
     if (block_get_header(malloc_1)->is_free || block_get_header(malloc_2)->is_free) {
         printf("Test 2 failed: something went wrong, one or both blocks are empty. \n");
+        _free(malloc_1);
+        _free(malloc_2);
         return false;
     }
 
@@ -43,6 +47,7 @@ bool test_2(void * start_heap) {
 
     if (!block_get_header(malloc_1)->is_free) {
         printf("Test 2 failed: first block hadn't been freed. \n");
+        _free(malloc_2);
         return false;
     }
 
@@ -56,12 +61,64 @@ bool test_2(void * start_heap) {
         printf("Test 2 failed: both blocks had been freed. \n");
         return false;
     }
+    printf("Test 2 failed: unexpected error\n");
     return false;
 }
 
 // Освобождение двух блоков из нескольких выделенных.
 bool test_3(void * start_heap) {
+    printf("Test 3: Freeing two blocks from several allocated ones...\n");
 
+    void * malloc_1 = _malloc(100);
+    void * malloc_2 = _malloc(100);
+    void * malloc_3 = _malloc(100);
+
+    if (malloc_1 == NULL) {
+        printf("Test 3 failed: first of three blocks didn't allocate. \n");
+        _free(malloc_2);
+        _free(malloc_3);
+        return false;
+    }
+
+    if (malloc_2 == NULL) {
+        printf("Test 3 failed: second of three blocks didn't allocate. \n");
+        _free(malloc_1);
+        _free(malloc_3);
+        return false;
+    }
+
+    if (malloc_3 == NULL) {
+        printf("Test 3 failed: last of three blocks didn't allocate. \n");
+        _free(malloc_1);
+        _free(malloc_2);
+        return false;
+    }
+
+    if (block_get_header(malloc_1)->is_free || block_get_header(malloc_2)->is_free || block_get_header(malloc_3)->is_free) {
+        printf("Test 3 failed: something went wrong, one or all blocks are empty. \n");
+        _free(malloc_1);
+        _free(malloc_2);
+        _free(malloc_3);
+        return false;
+    }
+
+    _free(malloc_1);
+    _free(malloc_2);
+
+    if (block_get_header(malloc_1)->is_free && block_get_header(malloc_2)->is_free) {
+        if (block_get_header(malloc_3)->is_free) {
+            printf("Test 3 failed: three blocks had been freed, not two. \n");
+            return false;
+        } else {
+            printf("Test 3 passed! \n");
+            _free(malloc_3);
+            return true;
+        }
+    } else {
+        printf("Test 3 failed: first and second blocks hadn't been freed. \n");
+        _free(malloc_3);
+        return false;
+    }
 }
 
 // Память закончилась, новый регион памяти расширяет старый.
